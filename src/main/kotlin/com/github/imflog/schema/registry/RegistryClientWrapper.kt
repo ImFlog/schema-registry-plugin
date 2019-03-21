5a28d34fd4dcfc8be9a6899d0627e6a125af693d
Gajula, Vishnu (Contractor) <Vishnu.Gajula@nordstrom.com>
2019-03-20 17:46:07 -0700

    constant update

--- src/main/kotlin/com/github/imflog/schema/registry/RegistryClientWrapper.kt
+++ src/main/kotlin/com/github/imflog/schema/registry/RegistryClientWrapper.kt
@@ -15,19 +15,24 @@ object RegistryClientWrapper {
 
     private var registryClient: SchemaRegistryClient? = null
 
-    private const val basicAuthSourceUserInfo: String = "USER_INFO"
+    private const val BASIC_AUTH_SOURCE: String = "USER_INFO"
 
     fun client(url: String, auth: SchemaRegistryBasicAuth): SchemaRegistryClient? {
         if (registryClient == null) {
-            val config = HashMap<String, String>()
-            if (!auth.username.isNullOrEmpty() && !auth.password.isNullOrEmpty()) {
-                // Note that BASIC_AUTH_CREDENTIALS_SOURCE is not configurable as the plugin only supports
-                // a single schema registry URL, so there is no additional utility of the URL source.
-                config[SchemaRegistryClientConfig.BASIC_AUTH_CREDENTIALS_SOURCE] = basicAuthSourceUserInfo
-                config[SchemaRegistryClientConfig.USER_INFO_CONFIG] = auth.getBasicAuthCredentials()
-            }
-            registryClient = CachedSchemaRegistryClient(url, 100, config)
+
+            registryClient = CachedSchemaRegistryClient(url, 100, getConfig(auth))
         }
         return registryClient
+    }
+
+    fun getConfig(auth: SchemaRegistryBasicAuth): HashMap<String, String> {
+        val config = HashMap<String, String>()
+        if (!auth.username.isNullOrEmpty() && !auth.password.isNullOrEmpty()) {
+            // Note that BASIC_AUTH_CREDENTIALS_SOURCE is not configurable as the plugin only supports
+            // a single schema registry URL, so there is no additional utility of the URL source.
+            config[SchemaRegistryClientConfig.BASIC_AUTH_CREDENTIALS_SOURCE] = BASIC_AUTH_SOURCE
+            config[SchemaRegistryClientConfig.USER_INFO_CONFIG] = auth.getBasicAuthCredentials()
+        }
+        return config
     }
 }
