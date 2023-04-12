e1dba1c4c5830020d8c188bf17ac537cfcd6f048
ImFlog <garcia.florian.perso@gmail.com>
2023-04-12 18:11:23 +0200

    Prepare next iteration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.imflog"
-version = "1.10.0"
+version = "1.10.1-SNAPSHOT"
 
 
 plugins {
--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.github.imflog:kafka-schema-registry-gradle-plugin:1.10.0")
+        classpath("com.github.imflog:kafka-schema-registry-gradle-plugin:1.10.1-SNAPSHOT")
     }
 }
 
