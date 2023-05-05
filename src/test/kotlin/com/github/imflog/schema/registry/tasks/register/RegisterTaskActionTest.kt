d82645e632e8ce4e6f8e41ebf953c52b0ec184f2
Knauf <christoph.knauf@cbc-extern.de>
2023-05-05 13:22:00 +0200

    revert AvroSchemaParser.kt to version 1.8.0

--- src/test/kotlin/com/github/imflog/schema/registry/tasks/register/RegisterTaskActionTest.kt
+++ src/test/kotlin/com/github/imflog/schema/registry/tasks/register/RegisterTaskActionTest.kt
@@ -335,8 +335,8 @@ class RegisterTaskActionTest {
         val projectDirAbsolutePath = Paths.get("").toAbsolutePath().toString()
         val schemaPath = "$projectDirAbsolutePath/src/test/resources/"
         val subjects = listOf(
-            Subject("test", "${schemaPath}A.avsc", "AVRO")
-                .addLocalReference("B", "${schemaPath}B.avsc")
+            Subject("test", "${schemaPath}testSubject.avsc", "AVRO")
+                .addLocalReference("B", "${schemaPath}testType.avsc")
         )
 
         // When
