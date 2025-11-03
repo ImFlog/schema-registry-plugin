14112177655cf36da21844f938dc12c9504e0805
ImFlog <garcia.florian.perso@gmail.com>
2025-11-03 21:45:36 +0100

    chore: update junit dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,9 +40,10 @@ java {
 // Unit tests
 dependencies {
     testImplementation(gradleTestKit())
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
+    testImplementation("org.junit.platform:junit-platform-launcher:6.0.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:6.0.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:6.0.1")
     testImplementation("org.assertj:assertj-core:3.27.6")
     testImplementation("io.mockk:mockk:1.14.6")
 }
