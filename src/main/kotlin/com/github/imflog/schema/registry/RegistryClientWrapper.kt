81034f25868c959ec18bd6a8803d40c0c0790595
mikemuske <mikemuske@users.noreply.github.com>
2026-06-01 13:57:56 -0500

    Add support for HTTP headers to be configured on the CSR client

--- src/main/kotlin/com/github/imflog/schema/registry/RegistryClientWrapper.kt
+++ src/main/kotlin/com/github/imflog/schema/registry/RegistryClientWrapper.kt
@@ -13,11 +13,21 @@ import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchemaProvider
  */
 object RegistryClientWrapper {
 
+    private const val HEADER_PREFIX = "request.header."
+
     fun client(url: String, config: Map<String, String>): SchemaRegistryClient =
         CachedSchemaRegistryClient(
             listOf(url),
             100,
             listOf(AvroSchemaProvider(), JsonSchemaProvider(), ProtobufSchemaProvider()),
             config,
+            buildHeaders(config)
         )
+
+    private fun buildHeaders(config: Map<String, String>): Map<String, String>? {
+        val headers = config
+            .filterKeys { it.startsWith(HEADER_PREFIX) }
+            .mapKeys { it.key.removePrefix(HEADER_PREFIX) }
+        return headers.ifEmpty { null }
+    }
 }
